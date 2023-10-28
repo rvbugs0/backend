ebc0a015136e2d4c42e574dee36f2aed14cd4df2
Ravi Ailani <rvbugs0@gmail.com>
2023-10-28 17:35:10 -0500

    update courses added

--- instructor/course/update_course.php
+++ instructor/course/update_course.php
@@ -1,13 +1,13 @@
 <?php
 // Include your database connection code
+require_once "../../DatabaseConnection.php"; // Replace with your database connection script
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
     $course_id = $_POST["course_id"];
     $name = $_POST["name"];
     $code = $_POST["code"];
     $schedule = $_POST["schedule"];
-    $user_id = $_POST["user_id"];
+    
 
     // Check if course_id is a positive integer
     if (!is_numeric($course_id) || $course_id <= 0 || $course_id != intval($course_id)) {
@@ -17,24 +17,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     elseif (strlen($name) < 2 || strlen($name) > 255 || strlen($code) < 4 || strlen($code) > 10) {
         echo json_encode(["success" => false, "message" => "Name should be between 2 and 255 characters, and code should be between 4 and 10 characters."]);
     }
-    // Check if user_id is a positive integer
-    elseif (!is_numeric($user_id) || $user_id <= 0 || $user_id != intval($user_id)) {
-        echo json_encode(["success" => false, "message" => "User ID should be a positive integer."]);
-    } else {
-        $sql = "UPDATE course SET name = ?, code = ?, schedule = ?, user_id = ? WHERE id = ?";
-        // Execute the SQL statement with appropriate bindings
-        // Handle errors and success messages
+     else {
+        // Update course details in the database
+        $query = "UPDATE course SET name = ?, code = ?, schedule = ? WHERE id = ?";
+        
+        // Get the database connection
+        $conn = DatabaseConnection::getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("sssi", $name, $code, $schedule, $course_id);
+        $result = $stmt->execute();
 
-
-        $success = true;
-        $randomValue = (rand(0, 1) == 1);
-    
-        if ($randomValue) {
-            $success = true;
-        } else {
-            $success = false;
-        }
-        if ($success) {
+        // Check for success or failure and return a response
+        if ($result) {
             echo json_encode(["success" => true, "message" => "Course details updated successfully."]);
         } else {
             echo json_encode(["success" => false, "message" => "Failed to update course details. Please try again later."]);
